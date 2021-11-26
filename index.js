465de31f798fcc0d881642754c5d8d496e787187
William-Wolke <60339769+William-Wolke@users.noreply.github.com>
2021-11-26 09:22:01 +0100

    Gitignore

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ MongoClient.connect(url, { useNewUrlParser: true })
         app.post('/create', (req, res) => {
             userCollection.insertOne(req.body)
                 .then(result => {
-                    console.log(result)
+                    console.log("User created")
                     res.redirect('/')
                 })
                 .catch(error => console.error(error))
@@ -38,39 +38,80 @@ MongoClient.connect(url, { useNewUrlParser: true })
         app.post('/login', (req, res) => {
             let name = req.body.name;
             let pass = req.body.pass;
-            let users = db.collection('Users').find({ "name": req.body.name }).toArray()
+            let users = db.collection('Users').find({ "name": name }).toArray()
 
                 .then(result => {
                     console.log(result[0].pass)
                     //console.log(result.pass, pass)
                     if (result[0].pass == pass) {
-                        console.log("YESS")
+                        console.log("Logged in")
                         res.redirect('/')
                     }
                     else {
-                        console.log("noo")
+                        console.log("Incorrect password or username")
                         res.redirect('/')
                     }
                 })
                 .catch(error => console.error(error))
-            });
+        });
 
-            app.post('/update', (req, res) => {
-                let name = req.body.name;
-                let pass = req.body.pass;
-                let users = db.collection('Users').find({"name": name}).toArray()
-                    .then(result => {
-                        if (users.pass == req.body.pass) {
-                            console.log('yes');
-                            res.redirect('/')
-                            updateUserPass(req.body.name, req.body.newPass);
-                        }
-                    })
-                    .catch(error => console.error(error))
-            });
+        app.post('/update', (req, res) => {
+            let name = req.body.name;
+            let oldPass = req.body.oldPass;
+            let newPass = req.body.newPass;
+            let users = db.collection('Users').find({ "name": name }).toArray()
 
-            app.put(updateUserPass = (name, newPass) => {
-                let passChange = db.collection('Users').updateOne(name, { $set: { pass: newPass } })
-            });
-        })
-            .catch(error => console.error(error))
+                .then(result => {
+                    console.log(result[0].pass)
+                    //console.log(result.pass, pass)
+                    if (result[0].pass == oldPass) {
+                        console.log("Correct credentials")
+                        updateUserPass(result[0].name, newPass);
+                        res.redirect('/')
+                    }
+                    else {
+                        console.log("Incorrect password or username")
+                        res.redirect('/')
+                    }
+                })
+                .catch(error => console.error(error))
+        });
+
+        app.post('/delete', (req, res) => {
+            let name = req.body.name;
+            let pass = req.body.pass;
+            let users = db.collection('Users').find({ "name": name }).toArray()
+
+                .then(result => {
+                    console.log(result[0].pass)
+                    //console.log(result.pass, pass)
+                    if (result[0].pass == pass) {
+                        console.log("Correct credentials")
+                        deleteUser(name);
+                        res.redirect('/')
+                    }
+                    else {
+                        console.log("Incorrect password or username")
+                        res.redirect('/')
+                    }
+                })
+                .catch(error => console.error(error))
+        });
+
+        app.put(updateUserPass = (name, newPass) => {
+            let passChange = db.collection('Users').updateOne({ "name": name }, { $set: { pass: newPass } })
+            .then(result => {
+                console.log("Password changed")
+            })
+            .catch(error => console.error(error));
+        });
+
+        app.delete(deleteUser = (name) => {
+            let deleteUser = db.collection('Users').deleteOne({ "name": name })
+            .then(result => {
+                console.log("User deleted")
+            })
+            .catch(error => console.error(error));
+        });
+    })
+    .catch(error => console.error(error))
